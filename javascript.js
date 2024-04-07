ec61846940076996b2ad2e109b3ff1b45f6bfb64
Htoo Aung <kyawaung0924@gmail.com>
2024-04-06 20:32:00 -0600

    implemented functions, fully working console game. Documented

--- javascript.js
+++ javascript.js
@@ -1,77 +1,84 @@
-function playGame() {
-    let playerWins = 0;
-    let computerWins = 0;
+let playerWins = 0;
+let computerWins = 0;
+let ties = 0;
 
+/**
+ * Plays a game of rock, paper, scissors.
+ */
+function playGame() {
     for (let i = 0; i < 5; i++) {
-        playRound();
-        
-        if (returnWin()) {
+        let playerSelection = prompt("Choose: Rock, Paper, or Scissors");
+        let computerSelection = getComputerChoice();
+        let results = playRound(playerSelection, computerSelection);
+
+        if (results === 'win') {
             playerWins++;
+            console.log(`You win! ${playerSelection.toLowerCase()} beats ${computerSelection}`);
+        }
+        else if (results === "lose") {
+            computerWins++;
+            console.log(`You lose! ${computerSelection} beats ${playerSelection.toLowerCase()}`);
         }
         else {
-            computerWins++;
+            ties++;
+            console.log(`It's a tie! You both chose ${playerSelection.toLowerCase()}`);
         }
     }
+
+    decideWinner();
 }
 
+/**
+ * Plays a round of rock, paper, scissors.
+ * 
+ * @param {string} playerSelection 
+ * @param {string} computerSelection 
+ * @returns String value indicating a "win", "lose", or "tie".
+ */
 function playRound(playerSelection, computerSelection) {
     let pChoice = playerSelection;
     let cChoice = computerSelection;
-    
-    if (pChoice === "rock") {
 
-        if (cChoice === "scissors") {
-            return returnWin(pChoice, cChoice);
+    // Check to see the outcome
+    if (pChoice.toLowerCase() === "rock" && cChoice === "scissors" ||
+        pChoice.toLowerCase() === "paper" && cChoice === "rock" ||
+        pChoice.toLowerCase() === "scissors" && cChoice === "paper") {
+            return "win";
         }
-        else if (cChoice === "paper") {
-            return returnLose(pChoice, cChoice);
-        }
-        else {  
-            return returnTie(pChoice);
-        }
+    else if (pChoice === cChoice) {
+        return "tie";
     }
-    else if (pChoice === "paper") {
-
-        if (cChoice === "rock") {
-            return returnWin(pChoice, cChoice);
-        }
-        else if (cChoice === "scissors") {
-            return returnLose(pChoice, cChoice);
-        }
-        else {  
-            return returnTie(pChoice);
-        }
-    }
-
     else {
-
-        if (cChoice === "paper") {
-            return returnWin(pChoice, cChoice);
-        }
-        else if (cChoice === "rock") {
-            return returnLose(pChoice, cChoice);
-        }
-        else {  
-            return returnTie(pChoice);
-        }
+        return "lose";
     }
-    
 }
 
+/**
+ * Randomly selects a choice of rock, paper, or scissors from an array.
+ * 
+ * @returns An choice of rock, paper, or scissors
+ */
 function getComputerChoice() {
     let choice = ["rock", "paper", "scissors"];
 
     return choice[Math.floor(Math.random() * choice.length)];
 }
 
-function returnWin(winner, loser) {
-    return `You Won! ${winner} beats ${loser}!`;
+/**
+ * Checks who has more wins, or equal amount of wins.
+ * 
+ * @returns Console output of string declaring winner. 
+ */
+function decideWinner() {
+    if (playerWins > computerWins) {
+        return console.log("You win!");
+    }
+    else if (computerWins > playerWins) {
+        return console.log("You lose :(");
+    }
+    else {
+        return console.log("It's a tie.");
+    }
 }
 
-function returnLose(winner, loser) {
-    return `You Lose! ${winner} beats ${loser}!`;
-}
-
-function returnTie(tie) {
-    return `It's a tie! You both chose ${tie1}`;
-}
+playGame();
